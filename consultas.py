3d8c5f339feecee966864c80d85b748f26f3a5c6
Marcel <mrizzo25@gmail.com>
2023-05-19 20:46:49 -0300

    add get_last_block

--- consultas.py
+++ consultas.py
@@ -4,11 +4,25 @@ from dotenv import load_dotenv
 import os
 
 load_dotenv()
-
 bscscan_api_key = os.getenv('api_key')
 
-url = f'https://api.bscscan.com/api?module=account&action=txlistinternal&address=0x0000000000000000000000000000000000001004&startblock=0&endblock=999999999&sort=desc&apikey={bscscan_api_key}'
-response = requests.get(url)
-data = response.json()
+url_base = 'https://api.bscscan.com/api?module'
 
-print(data)
+def contract_internal():
+    url = f'{url_base}=txlistinternal&address=0x0000000000000000000000000000000000001004&startblock=0&endblock=999999999&sort=desc&apikey={bscscan_api_key}'
+    response = requests.get(url)
+    data = response.json()
+    return data
+
+def get_last_block():
+    url = f'{url_base}=proxy&action=eth_blockNumber&apikey={bscscan_api_key}'
+    response = requests.get(url)
+    data = response.json()
+    return data
+
+block = get_last_block()
+print(block)
+
+
+
+
