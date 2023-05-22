e80c6753db51a1cd79e28cdf2dd1c3a621f67ede
Marcel <mrizzo25@gmail.com>
2023-05-22 17:52:16 -0300

    added function - check gas limit

--- token-analysis/web3.py
+++ token-analysis/web3.py
@@ -12,3 +12,14 @@ def is_honeypot(address):
         return True
 
 result = is_honeypot(contract_address)
+
+def check_gas(contract):
+    low_gas_limit = 10000
+    for function in contract.abi:
+        if function['type'] == 'function':
+            gas_limit = function['gas']
+            if gas_limit < low_gas_limit:
+                return True
+    return False
+
+result = is_honeypot(contract_address)
